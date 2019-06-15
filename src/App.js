1e98e068ef9b95aef035177788813fdc6c5cc38f
jebediahelliott <jebediahelliott@gmail.com>
2019-06-15 19:59:14 -0400

    Setup routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import BreweryList from './BreweryList';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <BreweryList />
+      <Router>
+          <Route path='/' exact component={BreweryList} />
+          <Route path='/:brewery' render={({location}) => console.log(location.state)} />
+      </Router>
     </div>
   );
 }
--- src/BreweryCard.js
+++ src/BreweryCard.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import { Card } from 'react-bootstrap';
+import { Route, Link, withRouter } from 'react-router-dom';
 
 class BreweryCard extends Component {
 
   render(){
     const brewery = this.props.brewery;
+    console.log(this.props);
     return(
-      <Card style={{ width: '100%' }}>
-        <Card.Body>
-          <Card.Link href="#">
-            <Card.Title>{brewery.name}</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">{brewery.brewery_type}</Card.Subtitle>
-            <Card.Text>
-            {brewery.street} {brewery.city}, {brewery.state}, {brewery.postal_code}, {brewery.country}
-            </Card.Text>
-          </Card.Link>
-          <Card.Link href={brewery.website_url}>{brewery.website_url}</Card.Link>
-        </Card.Body>
-      </Card>
+      <div>
+        <Card style={{ width: '100%' }}>
+          <Card.Body>
+            <Link to={{
+              pathname: `/${brewery.name}`,
+              state: brewery
+            }}>
+              <Card.Title>{brewery.name}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">{brewery.brewery_type}</Card.Subtitle>
+              <Card.Text>
+              {brewery.street} {brewery.city}, {brewery.state}, {brewery.postal_code}, {brewery.country}
+              </Card.Text>
+            </Link>
+            <Card.Link href={brewery.website_url}>{brewery.website_url}</Card.Link>
+          </Card.Body>
+        </Card>
+      </div>
     )
   }
 }
 
-export default BreweryCard;
+export default withRouter(BreweryCard);
