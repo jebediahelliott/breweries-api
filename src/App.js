625eb048621d610df5dc36dfbaeec19ffa1e02a6
jebediahelliott <jebediahelliott@gmail.com>
2019-06-15 13:52:21 -0400

    Add search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Search from './Search';
 
 function App() {
   return (
     <div className="App">
-      
+      <Search />
     </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
 
 class Search extends Component {
 
+
+
   render() {
     return (
-
+      <div>
+      <InputGroup size="lg">
+        <InputGroup.Prepend>
+          <InputGroup.Text id="inputGroup-sizing-lg">Choose A City</InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
+        <InputGroup.Append>
+          <Button variant="secondary">Submit</Button>
+        </InputGroup.Append>
+      </InputGroup>
+      </div>
     )
   }
 }
