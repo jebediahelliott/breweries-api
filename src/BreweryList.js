4eb9b3f693f5ef54c6792be51779926338adf8c3
jebediahelliott <jebediahelliott@gmail.com>
2019-06-15 17:05:12 -0400

    Set up BreweryList to fetch breweries

--- src/BreweryList.js
+++ src/BreweryList.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import { Card } from 'react-bootstrap';
+import axios from 'axios';
 
 class BreweryList extends Component {
+  constructor() {
+    super()
+    this.state = {
+      breweries: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.openbrewerydb.org/breweries?by_city=harrisburg')
+    .then(res => this.setState({
+      breweries: res.data
+    }))
+    .catch(res => console.log(res.errors))
+  }
 
   render(){
     return(
       <Card style={{ width: '100%' }}>
         <Card.Body>
-          <Card.Title>Breweries in City</Card.Title>
+          <Card.Title>Breweries of Harrisburg Pennsylvania</Card.Title>
         </Card.Body>
       </Card>
     )
