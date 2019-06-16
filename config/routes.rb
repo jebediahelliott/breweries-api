941e899ffc73333c05807d280f2a2019511c6b0a
jebediahelliott <jebediahelliott@gmail.com>
2019-06-16 18:46:32 -0400

    Set up for deployment to Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :breweries, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :breweries, only: [:index]
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
