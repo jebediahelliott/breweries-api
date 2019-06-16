f36b926e16924b99e57903082082e4a16f197148
jebediahelliott <jebediahelliott@gmail.com>
2019-06-15 21:52:54 -0400

    Integrate Google Maps

--- src/Brewery.js
+++ src/Brewery.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
+import GoogleMapReact from 'google-map-react';
+import MapMarker from './MapMarker'
+
 
 class Brewery extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 15
+  };
 
   render() {
     const brewery = this.props.location.state
+    console.log(brewery);
     return (
       <div>
         <Card style={{ width: '100%' }}>
           <Card.Body>
-            <Link to='/'>Return to Breweries</Link>
+            <Link to='/'>Back to Breweries</Link>
               <Card.Title>{brewery.name}</Card.Title>
               <Card.Subtitle className="mb-2 text-muted">{brewery.brewery_type}</Card.Subtitle>
               <Card.Text>
@@ -19,6 +30,22 @@ class Brewery extends Component {
             <Card.Link href={brewery.website_url}>{brewery.website_url}</Card.Link>
           </Card.Body>
         </Card>
+        <div style={{ height: '50vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyBBnzoirznlUb-HBklwUM9U_qvLg6O09_Q' }}
+          defaultCenter={{
+            lat: parseFloat(brewery.latitude),
+            lng: parseFloat(brewery.longitude)
+          }}
+          defaultZoom={this.props.zoom}
+        >
+          <MapMarker
+            lat={parseFloat(brewery.latitude)}
+            lng={parseFloat(brewery.longitude)}
+            text={brewery.name}
+            />
+        </GoogleMapReact>
+        </div>
       </div>
     )
   }
