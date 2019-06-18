7dc0abf827d8f3985b2808a80bc603c12e86a27d
jebediahelliott <jebediahelliott@gmail.com>
2019-06-17 22:35:51 -0400

    test ENV variables

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,7 +1,7 @@
 class BreweriesController < ApplicationController
   def index
     @breweries = Brewery.all
-
+    puts ENV['MAP_KEY']
     render json: { breweries: @breweries, google_map: ENV['MAP_KEY'] }
   end
 end
