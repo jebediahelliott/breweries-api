13163d1800523cf09a206b4f3b14d24712e4b7ec
jebediahelliott <jebediahelliott@gmail.com>
2019-06-19 15:31:19 -0400

    Restrict CORS origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'still-atoll-15799.herokuapp.com/'
 
     resource '*',
       headers: :any,
