abddb389c5637dc2c9786f2ab67cd36b3b9547ee
jebediahelliott <jebediahelliott@gmail.com>
2019-06-19 16:13:40 -0400

    Remove CSRF measure

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,12 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'still-atoll-15799.herokuapp.com/'
+    origins 'still-atoll-15799.herokuapp.com/', 'localhost:3000'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
-
-Rails.application.config.action_controller.forgery_protection_origin_check = false
