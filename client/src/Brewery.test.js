48f72ae5be42454cda758518e73cdb2ba297646d
jebediahelliott <jebediahelliott@gmail.com>
2019-06-20 14:24:21 -0400

    Add test for props passed to GoogleMapReact

--- client/src/Brewery.test.js
+++ client/src/Brewery.test.js
@@ -16,7 +16,23 @@ describe('<Brewery />', () => {
     );
     expect(wrapper.find('BreweryCard').props().withMap).toEqual(true)
   })
-  // it('renders GoogleMapReact with the right location', () => {
-  //
-  // })
-})
+    it('renders GoogleMapReact with the right location', () => {
+      const location = {state: {
+        brewery: {
+          latitude: '0',
+          longitude: '0'
+        },
+        googleMap: ''
+      }}
+      const wrapper = mount(
+        <MemoryRouter>
+          <Brewery location={location}/>
+        </MemoryRouter>
+      );
+      expect(wrapper.find('GoogleMap').props().defaultCenter).toEqual({lat: 0, lng: 0})
+    })
+  })
+
+// Assertions for MapMarker
+  // expect(wrapper.find('MapMarker').props().lat).toEqual(0)
+  // expect(wrapper.find('MapMarker').props().lng).toEqual(0)
